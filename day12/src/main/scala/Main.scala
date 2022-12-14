412c3dfe5e2ab642c34215f7fa23bef524edcf7f
RuslanUchan <ruslanuchan97@gmail.com>
2022-12-14 14:30:37 +0700

    trying to refactor hmm

--- day12/src/main/scala/Main.scala
+++ day12/src/main/scala/Main.scala
@@ -7,18 +7,18 @@ import scala.annotation.tailrec
   val sampleCase = inputFileLoader("/input_sample")
   val testCase   = inputFileLoader("/input")
 
-  // val pt1Sample = part1(sampleCase)
-  // val pt1Test   = part1(testCase)
+  val pt1Sample = part1(sampleCase)
+  val pt1Test   = part1(testCase)
 
-  // println("--- First case ---")
-  // println(s"sample: $pt1Sample")
-  // println(s"actual: $pt1Test")
+  println("--- First case ---")
+  println(s"sample: $pt1Sample")
+  println(s"actual: $pt1Test")
 
-  val pt2Sample = part2(sampleCase)
+  // val pt2Sample = part2(sampleCase)
   // val pt2Test   = part2(testCase)
 
-  println("\n--- Second input ---")
-  println(s"sample: $pt2Sample")
+  // println("\n--- Second input ---")
+  // println(s"sample: $pt2Sample")
   // println(s"actual: $pt2Test")
 
 type Grid = Vector[Vector[Char]]
@@ -46,6 +46,19 @@ def move = (grid: Grid) =>
   (current: Coordinate, target: Coordinate) =>
     if (grid(target.y)(target.x) - grid(current.y)(current.x)) <= 1 then Some(target) else None
 
+def paths = (grid: Grid) =>
+  (coord: Coordinate) =>
+    val colLength   = grid(0).length
+    val rowLength   = grid.length
+    def checkBounds = bounds(rowLength, colLength)
+    def checkMove   = move(grid)
+    def getPossiblePaths = generatePathways(coord)
+      .map(checkBounds)
+      .flatten
+      .map(checkMove(coord, _))
+      .flatten
+    getPossiblePaths
+
 def prep(data: List[String]): (Grid, Coordinate, Coordinate) =
   val input: Grid = data
     .map(_.split("").map(_.charAt(0)).toVector)
@@ -66,76 +79,66 @@ def prep(data: List[String]): (Grid, Coordinate, Coordinate) =
 
   (testCase, start, end)
 
-def run(testCase: Grid, start: Coordinate, end: Coordinate): Map[Coordinate, Int] =
-  val colLength = testCase(0).length
-  val rowLength = testCase.length
+@annotation.tailrec
+def traverse(
+    q: Seq[Step],
+    v: Set[Coordinate],
+    accumulator: Map[Coordinate, Int],
+    computePossiblePaths: Coordinate => List[Coordinate]
+): Map[Coordinate, Int] =
+  if q.isEmpty then accumulator
+  else
+    val next          = q.head
+    val possiblePaths = computePossiblePaths(next.c)
 
-  def checkBounds = bounds(rowLength, colLength)
-  def checkMove   = move(testCase)
+    val possiblePathsCosts = possiblePaths
+      .map(Step(_, next.count + 1))
+      .toSet
+
+    val checkMinCost = for path <- possiblePathsCosts yield
+      val costToPossiblePath = accumulator.getOrElse(path.c, 9999)
+      if path.count < costToPossiblePath then Some(path)
+      else None
+
+    val succ       = checkMinCost.flatten.filterNot(q.contains(_))
+    val updatedAcc = accumulator + (next.c -> next.count)
+
+    if succ.isEmpty then traverse(q.tail, v + next.c, updatedAcc, computePossiblePaths)
+    else traverse(q.tail ++ succ, v + next.c, updatedAcc, computePossiblePaths)
+
+def traverseFrom(initial: Step, computePossiblePaths: Coordinate => List[Coordinate]) =
+  traverse(Seq(initial), Set.empty, Map(initial.c -> 0), computePossiblePaths)
+
+def part1(data: List[String]): Int =
+  val (grid, start, end) = prep(data)
 
   println(s"Start: $start")
   println(s"End: $end")
 
-  @annotation.tailrec
-  def traverse(
-      q: Seq[Step],
-      v: Set[Coordinate],
-      accumulator: Map[Coordinate, Int]
-  ): Map[Coordinate, Int] =
-    if q.isEmpty then accumulator
-    else
-      val next = q.head
-      val possiblePaths = generatePathways(next.c)
-        .map(checkBounds)
-        .flatten
-        .map(checkMove(next.c, _))
-        .flatten
-
-      val possiblePathsCosts = possiblePaths
-        .map(Step(_, next.count + 1))
-        .toSet
-
-      val checkMinCost = for path <- possiblePathsCosts yield
-        val costToPossiblePath = accumulator.getOrElse(path.c, 9999)
-        if path.count < costToPossiblePath then Some(path)
-        else None
-
-      val succ       = checkMinCost.flatten.filterNot(q.contains(_))
-      val updatedAcc = accumulator + (next.c -> next.count)
-
-      if succ.isEmpty then traverse(q.tail, v + next.c, updatedAcc)
-      else traverse(q.tail ++ succ, v + next.c, updatedAcc)
-
-  def traverseFrom(initial: Step) =
-    traverse(Seq(initial), Set.empty, Map(initial.c -> 0))
-
-  traverseFrom(Step(start, 0))
-
-def part1(data: List[String]): Int =
-  val (grid, start, end) = prep(data)
-  val res                = run(grid, start, end)
+  val getPossiblePaths = paths(grid)
+  val res              = traverseFrom(Step(start, 0), getPossiblePaths)
   res(end)
 
-def part2(data: List[String]): Int =
-  val (grid, start, end) = prep(data)
-  val res                = run(grid, start, end)
+// def part2(data: List[String]): Int =
+//   val (grid, start, end) = prep(data)
+//   val res                = run(grid, start, end)
 
-  // consider all a
-  val allAs = for
-    y <- 0 to grid.length - 1
-    x <- 0 to grid(0).length - 1
-    if grid(y)(x) == 'a'
-  yield
-    val current = Coordinate(x, y)
-    if res.isDefinedAt(current) then res(end) - res(current)
-  15
+//   // consider all a
+//   val allAs = for
+//     y <- 0 to grid.length - 1
+//     x <- 0 to grid(0).length - 1
+//     if grid(y)(x) == 'a'
+//   yield
+//     val current = Coordinate(x, y)
+//     if res.isDefinedAt(current) then res(end) - res(current)
+//   15
 
-  // val lowestElevation = res.foldLeft(0) { (highestCost, m) =>
-  //   val (k, v) = m
-  //   if grid(k.y)(k.x) == 'a' then if v > highestCost then v else highestCost
-  //   else highestCost
-  // }
-  // res(end) - lowestElevation
+// val lowestElevation = res.foldLeft(0) { (highestCost, m) =>
+//   val (k, v) = m
+//   if grid(k.y)(k.x) == 'a' then if v > highestCost then v else highestCost
+//   else highestCost
+// }
+// res(end) - lowestElevation
 
 def inputFileLoader(filename: String): List[String] =
   Source
