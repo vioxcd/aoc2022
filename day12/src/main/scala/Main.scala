e463a5bcd4ea66c706b670c1ee2dd535c0aebb5d
RuslanUchan <ruslanuchan97@gmail.com>
2022-12-14 12:01:58 +0700

    DAY 12 YAY! there's a bug in checking visited: should also check whether current visit is minimal in terms of cost!

--- day12/src/main/scala/Main.scala
+++ day12/src/main/scala/Main.scala
@@ -5,17 +5,17 @@ import scala.annotation.tailrec
 
 @main def main: Unit =
   val sampleCase = inputFileLoader("/input_sample")
-  // val testCase   = inputFileLoader("/input")
+  val testCase   = inputFileLoader("/input")
 
   val pt1Sample = part1(sampleCase)
-  // val pt1Test   = part1(testCase)
+  val pt1Test   = part1(testCase)
 
   println("--- First case ---")
   println(s"sample: $pt1Sample")
-  // println(s"actual: $pt1Test")
+  println(s"actual: $pt1Test")
 
-  // val pt2Sample = part2(sampleMonkeys)
-  // val pt2Test = part2(testMonkeys)
+  // val pt2Sample = part2(sampleCase)
+  // val pt2Test = part2(testCase)
 
   // println("\n--- Second input ---")
   // println(s"sample: $pt2Sample")
@@ -84,40 +84,54 @@ def part1(data: List[String]): Int =
   println(s"End: $end")
 
   @annotation.tailrec
-  def traverse(g: Grid, q: Seq[Step], v: Set[Coordinate], accumulator: Seq[Step], end: Coordinate): Seq[Step] =
+  def traverse(
+      g: Grid,
+      q: Seq[Step],
+      v: Set[Coordinate],
+      accumulator: Map[Coordinate, Int]
+      // end: Coordinate
+  ): Map[Coordinate, Int] =
     if q.isEmpty then accumulator
     else
       val next = q.head
-      if next == end then
-        println("Finished!")
-        accumulator :+ Step(next.c, next.count + 1)
+      // if next == end then
+      //   println("Finished!")
+      //   accumulator :+ Step(next.c, next.count + 1)
 
       val possiblePaths = generatePathways(next.c)
         .map(checkBounds)
         .flatten
         .map(checkMove(next.c, _))
         .flatten
-        .filterNot((c: Coordinate) => v(c))
+        .map(Step(_, next.count + 1))
+        .toSet
 
-      if possiblePaths.isEmpty then
+      // .filterNot(q.contains(_))
+      // .filterNot((c: Coordinate) => v(c))
+
+      val checkMinCost = for path <- possiblePaths yield
+        val costToPossiblePath = accumulator.getOrElse(path.c, 9999)
+        if path.count < costToPossiblePath then
+          println(s"Update ${path.count} from ${costToPossiblePath}")
+          Some(path)
+        else None
+
+      val succ       = checkMinCost.flatten.filterNot(q.contains(_))
+      val updatedAcc = accumulator + (next.c -> next.count)
+
+      if succ.isEmpty then
         println("empty!")
-        traverse(g, q.tail, v + next.c, accumulator, end)
-      else
-        val succ = possiblePaths
-          .map(Step(_, next.count + 1))
-          .toSet
-          .filterNot(q.contains(_))
-        traverse(g, q.tail ++ succ, v + next.c, accumulator :+ next, end)
+        traverse(g, q.tail, v + next.c, updatedAcc)
+        // traverse(g, q.tail, v + next.c, accumulator, end)
+      // else traverse(g, q.tail ++ succ, v + next.c, accumulator + (next.c -> next.count), end)
+      else traverse(g, q.tail ++ succ, v + next.c, updatedAcc)
 
-  def traverseFrom(g: Grid, initial: Step, end: Coordinate) =
-    traverse(g, Seq(initial), Set.empty, Seq.empty, end)
+  def traverseFrom(g: Grid, initial: Step) =
+    traverse(g, Seq(initial), Set.empty, Map(initial.c -> 0))
+    // traverse(g, Seq(initial), Set.empty, Seq.empty, end)
 
-  val res = traverseFrom(input, Step(start, 0), end)
-  println(res)
-  res.takeRight(1).head.count + 1
-
-// def part2(monkeys: Vector[Monkey]): Long =
-//   15
+  val res = traverseFrom(input, Step(start, 0))
+  res(end)
 
 def inputFileLoader(filename: String): List[String] =
   Source
